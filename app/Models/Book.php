af486d16006de8ffbbb97c2911f7d570e16febcb
Ammar <ammarasafi041@gmail.com>
2025-01-31 22:37:02 +0300

    .

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,13 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory,
+    Illuminate\Database\Eloquent\Model,
+    Illuminate\Database\Eloquent\SoftDeletes,
+    Mpociot\Versionable\VersionableTrait;
 
 class Book extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, VersionableTrait;
+
 
     protected $fillable = [
         "title",
